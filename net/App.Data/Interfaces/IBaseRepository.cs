f658827637bb57c08a003816e097aebfd7278e5b
Sebastian Górka <sebastiangorka442@gmail.com>
2022-10-22 15:56:57 +0200

    complete BaseRepository

--- net/App.Data/Interfaces/IBaseRepository.cs
+++ net/App.Data/Interfaces/IBaseRepository.cs
@@ -4,4 +4,8 @@ public interface IBaseRepository
 {
     Task<ICollection<TEntity>> GetAllAsync<TEntity, TKey>() where TEntity : class, IBaseEntity<TKey>;
     Task<TEntity> GetSingle<TEntity, TKey>(TKey id, CancellationToken cancellationToken) where TEntity : class, IBaseEntity<TKey>;
+    TEntity Add<TEntity, TKey>(TEntity entity) where TEntity : class, IBaseEntity<TKey>;
+    TEntity Update<TEntity, TKey>(TEntity entity) where TEntity : class, IBaseEntity<TKey>;
+    TEntity Remove<TEntity, TKey>(TEntity entity) where TEntity : class, IBaseEntity<TKey>;
+
 }
--- net/App.Data/Repositories/BaseRepository.cs
+++ net/App.Data/Repositories/BaseRepository.cs
@@ -14,11 +14,29 @@ public abstract class BaseRepository : IBaseRepository
 
     public async Task<ICollection<TEntity>> GetAllAsync<TEntity, TKey>() where TEntity : class, IBaseEntity<TKey>
     {
-        return await _appDbContext.Set<TEntity, TKey>().ToListAsync();
+        return await _appDbContext.Set<TEntity, TKey>().AsNoTracking().ToListAsync();
     }
 
     public async Task<TEntity> GetSingle<TEntity, TKey>(TKey id, CancellationToken cancellationToken) where TEntity : class, IBaseEntity<TKey>
     {
-        return await _appDbContext.Set<TEntity, TKey>().FirstOrDefaultAsync(cancellationToken);
+        return await _appDbContext.Set<TEntity, TKey>().AsNoTracking().FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public TEntity Add<TEntity, TKey>(TEntity entity) where TEntity : class, IBaseEntity<TKey>
+    {
+        var entityEntry = _appDbContext.Set<TEntity, TKey>().Add(entity);
+        return entityEntry.Entity;
+    }
+
+    public TEntity Update<TEntity, TKey>(TEntity entity) where TEntity : class, IBaseEntity<TKey>
+    {
+        var entityEntry = _appDbContext.Set<TEntity, TKey>().Update(entity);
+        return entityEntry.Entity;
+    }
+
+    public TEntity Remove<TEntity, TKey>(TEntity entity) where TEntity : class, IBaseEntity<TKey>
+    {
+        var entityEntry = _appDbContext.Set<TEntity, TKey>().Remove(entity);
+        return entityEntry.Entity;
     }
 }
