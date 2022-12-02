5044e2607ef020b4ec497c4855441b62a7ed24e5
dawid bieniek <01159194@pw.edu.pl>
2022-12-02 13:28:28 +0100

    Add test for repository controller

---
+++ net/App.Test/Controllers/ReviewControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using App.API.Controllers;
+using App.Data.Entities;
+using App.Data.Interfaces;
+
+using FakeItEasy;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Xunit;
+
+namespace App.Test.Controllers
+{
+	public class ReviewControllerTest
+	{
+		private readonly IReviewRepository _reviewRepository;
+
+		public ReviewControllerTest()
+		{
+			_reviewRepository = A.Fake<IReviewRepository>();
+		}
+
+		[Fact]
+		public async void ReviewController_AddReview_ReturnOk()
+		{
+			//Arrange
+			ReviewController controller = new(_reviewRepository);
+			Review review = new();
+
+			//Act
+			IActionResult result = await controller.AddReview(review);
+
+			//Assert
+			Assert.IsType<OkResult>(result);
+		}
+	}
+}
