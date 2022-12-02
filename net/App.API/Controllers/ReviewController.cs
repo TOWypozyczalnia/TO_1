8b31d81f4d6c5970d1d63db8b927ff90e5efa1d1
dawid bieniek <01159194@pw.edu.pl>
2022-12-02 14:11:54 +0100

    Add controller for reviews

--- net/App.API/Controllers/ReviewController.cs
+++ net/App.API/Controllers/ReviewController.cs
@@ -1,6 +1,6 @@
 using App.Data.Entities;
 using App.Data.Interfaces;
-using Microsoft.AspNetCore;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.API.Controllers;
@@ -9,18 +9,25 @@ namespace App.API.Controllers;
 [Route("api/[controller]/")]
 public class ReviewController : BaseController
 {
-    private readonly IReviewRepository _reviewRepository;
+	private readonly IReviewRepository _reviewRepository;
 
-    public ReviewController(IReviewRepository reviewRepository)
-    {
-        _reviewRepository = reviewRepository;
-    }
-        
-    [HttpPost("AddReview")]
-    public async Task<IActionResult> AddReview([FromBody] Review review)
-    {
-        // TODO: Sprawdzanie, czy film zosta³ kiedyœ wypo¿yczony przez u¿ytkownika
-        _reviewRepository.Add(review);
-        return Ok();
-    }
+	public ReviewController(IReviewRepository reviewRepository)
+	{
+		_reviewRepository = reviewRepository;
+	}
+
+	[HttpPost("AddReview")]
+	public async Task<IActionResult> AddReview([FromBody] Review review)
+	{
+		// TODO: Sprawdzanie, czy film zosta³ kiedyœ wypo¿yczony przez u¿ytkownika
+		try
+		{
+			_reviewRepository.Add(review);
+		}
+		catch (Microsoft.EntityFrameworkCore.DbUpdateException)
+		{
+			return BadRequest("Invalid movie ID");
+		}
+		return Ok();
+	}
 }
