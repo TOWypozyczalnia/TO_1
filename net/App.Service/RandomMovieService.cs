0b78da6567cc8be008803457445e37590c061c9a
AndrzejFFGrabowski <Andrzej.f.f.Grabowski@gmail.com>
2022-12-05 19:08:10 +0100

    test

---
+++ net/App.Service/RandomMovieService.cs
@@ -0,0 +1,19 @@
+﻿using App.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Service
+{
+    public class RandomMovieService
+    {
+        public Movie DrawMovie(List<Movie>movies)
+        {
+            Random random = new Random();
+            if (movies.Count == 0) return null;
+            return movies[random.Next(movies.Count())];
+        }
+    }
+}
